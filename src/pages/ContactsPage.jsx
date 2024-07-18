47db3ddc3d68046d5511de8ebc15a1ea2c4513ba
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-07-18 03:26:39 +0300

    Add favicons

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,6 +1,13 @@
 import Contacts from "../components/Contacts/Contacts";
+import phoneIco from "../../public/phone-favicon.ico";
+import { useEffect } from "react";
 
 const ContactsPage = () => {
+  useEffect(() => {
+    let link = document.querySelector("link[rel='shortcut icon']");
+    link.href = phoneIco;
+  }, []);
+
   return <Contacts />;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,15 @@ import Hero from "../components/Hero/Hero";
 import Skills from "../components/Skills/Skills";
 import TextSection from "../components/TextSection/TextSection";
 import Projects from "../components/Projects/Projects";
+import { useEffect } from "react";
+import homeIco from "../../public/house-favicon.ico";
 
 const Home = () => {
+  useEffect(() => {
+    let link = document.querySelector("link[rel='shortcut icon']");
+    link.href = homeIco;
+  }, []);
+
   return (
     <Fragment>
       <Hero />
